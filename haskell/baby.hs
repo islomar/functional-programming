27e1501b79565a068b9f068a7ea3dd846dfdd481
Isidro Lopez <islomar@gmail.com>
2015-10-29 14:43:31 +0100

    Remove duplication from guard

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -68,10 +68,11 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 -- GUARDS --
 bmiTell :: (RealFloat a) => a -> a -> String  
 bmiTell weight height  
-    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"  
-    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
-    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
-    | otherwise                 = "You're a whale, congratulations!"
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2
 --ghci> bmiTell 85 1.90  
 
 ---Guards can also be written inline, but it's less readable
