a17babbedfd9493247776f5988eb481134d9cf96
Isidro Lopez <islomar@gmail.com>
2015-10-29 18:51:17 +0100

    Improve readibility

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -68,11 +68,14 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 -- GUARDS --
 bmiTell :: (RealFloat a) => a -> a -> String  
 bmiTell weight height  
-    | bmi <= 18.5 = "You're underweight, you emo, you!"  
-    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
-    | otherwise   = "You're a whale, congratulations!"  
-    where bmi = weight / height ^ 2
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0
 --ghci> bmiTell 85 1.90  
 
 ---Guards can also be written inline, but it's less readable
