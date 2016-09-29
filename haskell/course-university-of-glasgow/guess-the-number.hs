1bf515c393a11aa2f3e8820eee37e947ad787b41
Isidro Lopez <islomar@gmail.com>
2016-09-29 19:39:22 +0200

    [haskell] Read the number of turns

--- haskell/course-university-of-glasgow/guess-the-number.hs
+++ haskell/course-university-of-glasgow/guess-the-number.hs
@@ -1,8 +1,10 @@
 -- single turn for user
-turn :: Int -> IO ()
-turn number =
-  do putStrLn ("You want to guess the number " ++ (show number))
+turn :: Int -> Int -> IO ()
+turn number turns =
+  do 
+    putStrLn ("You want to guess the number " ++ (show number))
+    putStrLn ("You have " ++ (show turns) ++ " turns")
 
 -- top-level function. Usage: starman "WORD" NUM_TURNS
-guess_number :: Int -> IO ()
-guess_number number = turn number
+guess_number :: Int -> Int -> IO ()
+guess_number number turns = turn number turns
