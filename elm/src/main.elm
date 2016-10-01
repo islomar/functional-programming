aa4071109bc62a5dec02f2939622af2ceb23617c
Isidro Lopez <islomar@gmail.com>
2016-10-01 15:47:35 +0200

    [elm] Use a Model

--- elm/src/main.elm
+++ elm/src/main.elm
@@ -2,10 +2,18 @@ module Test exposing (..)
 
 import Html exposing (div, text, h1)
 
+type alias Model =
+    { name : String
+    , counter : Int
+    }
+
+initialModel = {name = "ELM 101", counter = 3}
+
 view model =
-    div[][
-        h1[][text model]
+    div [][
+        h1 [][text model.name]
+        , div [][text (toString model.counter)]
     ]
 
 main =
-    view "Hello Barcelona!"
+    view initialModel
